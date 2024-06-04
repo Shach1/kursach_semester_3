5bfe34f0e326ba4d7164586a2abddc11d27d5e7d
Evgeniy PC <79169380904@mail.ru>
2024-06-04 07:36:44 +0300

    added: creating an order in the DB, basic theme

--- app/src/main/java/ru/mirea/androidcoursework/main/CartFragment.java
+++ app/src/main/java/ru/mirea/androidcoursework/main/CartFragment.java
@@ -11,9 +11,20 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import ru.mirea.androidcoursework.Decoration.SpaceItemDecoration;
 import ru.mirea.androidcoursework.adapter.CardProductPreviewInCartAdapter;
 import ru.mirea.androidcoursework.databinding.CartFragmentBinding;
+import ru.mirea.androidcoursework.entity.CardProductInCart;
 import ru.mirea.androidcoursework.instance.ProductsInCart;
 import ru.mirea.androidcoursework.interfaceses.IOnCartChangedListener;
 
@@ -27,6 +38,7 @@ public class CartFragment extends Fragment implements IOnCartChangedListener
 
     private CartFragmentBinding binding;
     private ProductsInCart productsInCart;
+    private CardProductPreviewInCartAdapter adapter;
 
     @Nullable
     @Override
@@ -46,17 +58,17 @@ public class CartFragment extends Fragment implements IOnCartChangedListener
         super.onViewCreated(view, savedInstanceState);
         Log.d("CartFragment", "onViewCreated: ");
 
-        initGlobal();
+        init();
     }
 
 
-    private void initGlobal()
+    private void init()
     {
         productsInCart = ProductsInCart.getInstance();
-
+        binding.btPurchase.setOnClickListener(this::onPurchase);
         binding.rvCartProducts.setLayoutManager(new LinearLayoutManager(getContext()));
         binding.rvCartProducts.addItemDecoration(new SpaceItemDecoration(20));
-        CardProductPreviewInCartAdapter adapter = new CardProductPreviewInCartAdapter(productsInCart.getProductsInCart(), this);
+        adapter = new CardProductPreviewInCartAdapter(productsInCart.getProductsInCart(), this);
         binding.rvCartProducts.setAdapter(adapter);
         updateTotalPrice();
     }
@@ -66,8 +78,56 @@ public class CartFragment extends Fragment implements IOnCartChangedListener
         binding.tvTotalPrice.setText(totalPrice + " ₽");
     }
 
+    private void onPurchase(View view)
+    {
+        if (productsInCart.getProductsInCart().isEmpty())
+        {
+            return;
+        }
+        List<Product> products = new ArrayList<>();
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail().replace(".", ",");
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
+        String date = sdf.format(new Date());
+
+        for (CardProductInCart productInCart : productsInCart.getProductsInCart())
+        {
+            products.add(new Product(productInCart.getCardProduct().getTitle(), productInCart.getAmount()));
+        }
+        Map<String, Product> productsMap = new HashMap<>();
+        for (Product product : products) {
+            String key = FirebaseDatabase.getInstance().getReference().child("orders").child(email).child(date).push().getKey();
+            productsMap.put(key, product);
+        }
+        FirebaseDatabase.getInstance().getReference().child("orders").child(email).child(date).setValue(productsMap);
+
+        productsInCart.clearProductsInCart();
+        updateTotalPrice();
+        if (adapter != null) {
+            adapter.notifyDataSetChanged();
+        }
+    }
+
     @Override
     public void onCartChanged() {
         updateTotalPrice();
+    }
+
+    private class Product
+    {
+        private String name;
+        private int amount;
+
+        public Product(String name, int amount) {
+            this.name = name;
+            this.amount = amount;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
     }
 }
